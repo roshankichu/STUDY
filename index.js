c2c084c6445309fa8f48f9665bbd5cee2d4f578b
roshan s s <roshanss101@gmail.com>
2023-01-31 12:38:30 +0530

    changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 const authRoutes = require("./routes/user.route");
 
-require("dotenv").configg();
+require("dotenv").config();
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
